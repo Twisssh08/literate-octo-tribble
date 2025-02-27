290395a89e0b6084bf372250f8a2a6ad1042352d
Mateo Acosta <twishm08@gmail.com>
2025-02-27 10:56:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -47,11 +47,11 @@ in_mod = st.selectbox(
   ("Auditive", "Visual", "Haptic"),
 )
 if in_mod == "Audítive":
-  set_mod = "IMAGINE A SOUND.... WAIT THERE"
+      set_mod = "IMAGINE A SOUND.... WAIT THERE"
 elif in_mod == "Visual":
-  set_mod = "REPRODUCIN A VIDEO... WAIT THERE"
+    set_mod = "REPRODUCIN A VIDEO... WAIT THERE"
 elif in_mod == "Haptic":
-  set_mod = "ACTIVATIN VIBRATION !!!"
+    set_mod = "ACTIVATIN VIBRATION !!!"
 st.write("THE ACTION IS... ", set_mod)
 
 with st.sidebar:
