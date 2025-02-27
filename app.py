f5f0082ee938b08ac603059d7e4b47fa2caa7a80
Mateo Acosta <twishm08@gmail.com>
2025-02-27 10:17:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Mi primera app!!")
 
 st.header("HOLY MOLY !!")
-sr.writer("raise your ya ya ya !")
+sr.write("raise your ya ya ya !")
 image = Image.open("HolyMoly.jpg")
 
 st.image(image, caption="holyyy god, this is a image")
