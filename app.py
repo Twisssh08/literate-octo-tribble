6577b94e07ef72d6fccf2c298d43c1b9b9122ecf
Mateo Acosta <twishm08@gmail.com>
2025-02-27 10:10:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
+from PIL import Image
+
 st.title("Mi primera app!!")
+
+st.header("HOLY MOLY !!")
+sr.writer("raise your ya ya ya !")
+Image = Image.open("HolyMoly.jpg")
+
+st.image(image, caption="holyyy god, this is a brainrot image")
