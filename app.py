3fe5ce3c326429781a756522943c3ed589372e0a
Mateo Acosta <twishm08@gmail.com>
2025-02-27 10:17:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Mi primera app!!")
 
 st.header("HOLY MOLY !!")
 sr.writer("raise your ya ya ya !")
-Image = Image.open("HolyMoly.jpg")
+image = Image.open("HolyMoly.jpg")
 
-st.image(image, caption="holyyy god, this is a brainrot image")
+st.image(image, caption="holyyy god, this is a image")
